a622c10bd51bcbfd54a28a2e4198f2177b6731d6
ZorinSr <izansimon2004@gmail.com>
2024-10-07 21:57:57 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 let x = 0;
 
+
 function addContent(x) {
     
     let limit = 3+x;
@@ -44,32 +45,70 @@ function sEnd(){
 
 function heaviWeather() {
 
-    let limit = 3;
     console.log("skividi");
 
-    fetch("https://api.open-meteo.com/v1/forecast?latitude=35.6895&longitude=139.6917&current_weather=true")
-    .then((response) => {
-        return  response.text();
-    })
-    .then((weather) => {
-        const weatherData = JSON.parse(weather);
-        console.log(weatherData);
+    let Madrid = "https://api.open-meteo.com/v1/forecast?latitude=40.4165&longitude=-3.70256&current_weather=true";
+    let Tokio = "https://api.open-meteo.com/v1/forecast?latitude=35.6895&longitude=139.6917&current_weather=true";
+    let Napoles = "https://api.open-meteo.com/v1/forecast?latitude=40.8563100&longitude=14.2464100&current_weather=true";
+    let Egipt = "https://api.open-meteo.com/v1/forecast?latitude=26.820553&longitude=30.802498&current_weather=true";
+    let NewYork = "https://api.open-meteo.com/v1/forecast?latitude=40.71427&longitude=-74.00597&current_weather=true";
+    let Loc = [Madrid, Tokio, Napoles, Egipt, NewYork];
+    let LocNm = ["Madrid", "Tokio", "Napoles", "Egipt", "NewYork"];
 
-        let container = document.getElementById("weatherReport");
-        container.innerHTML = "";
+    let container = document.getElementById("weatherReport");
+    container.innerHTML = "";
 
-        const article = document.createElement("article");
-        const timezone = document.createElement("h1");
-        timezone.textContent = weatherData.timezone;
-        const latitude = document.createElement("p");
-        latitude.textContent = weatherData.latitude;
-        const longitude = document.createElement("p");
-        longitude.textContent = weatherData.longitude;
+    for(let i = 0; i <= 4; i++){
 
-        article.appendChild(timezone)
-        article.appendChild(latitude);
-        article.appendChild(longitude);
-        container.appendChild(article);
+    
+        fetch(Loc[i])
+        .then((response) => {
+            return  response.text();
+        })
+        .then((weather) => {
+            const weatherData = JSON.parse(weather);
+            console.log(weatherData);
+
+            let container = document.getElementById("weatherReport");
+
+            const title = document.createElement("h1");
+            title.textContent = "↓ " + LocNm[i] + " ↓";
+            const article = document.createElement("article");
+
+            const temp = document.createElement("p");
+            temp.textContent = "Temperature : ";
+            temp.textContent += weatherData.current_weather.temperature;
+            temp.textContent += weatherData.current_weather_units.temperature;
+
+            const ws = document.createElement("p");
+            ws.textContent = "Wind Speed : ";
+            ws.textContent += weatherData.current_weather.windspeed;
+            ws.textContent += weatherData.current_weather_units.windspeed;
+
+
+            const wd = document.createElement("p");
+            wd.textContent = "Wind Direction : ";
+            wd.textContent += weatherData.current_weather.winddirection;
+            wd.textContent += weatherData.current_weather_units.winddirection;
+
+            const crTime = document.createElement("p");
+            crTime.textContent = "Last update : "
+            crTime.textContent += weatherData.current_weather.time;
+            crTime.textContent += " in " + weatherData.timezone;
         
-    });
+
+
+            container.appendChild(title);
+            article.appendChild(temp);
+            article.appendChild(ws);
+            article.appendChild(wd);
+            article.appendChild(crTime);
+            container.appendChild(article);
+            
+        });
+    }
 }
+
+
+
+setInterval('heaviWeather()', 30000);
